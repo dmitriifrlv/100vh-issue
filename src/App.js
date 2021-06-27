1355047381399e79b1b9aaf1501e068728af5745
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2021-06-27 20:54:34 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [height, setHeight] = useState(window.innerHeight);
+
+  const handleWindowSizeChange = () => {
+    setHeight(window.innerHeight);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => window.removeEventListener("resize", handleWindowSizeChange);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ height: height }}>
+      <header className="top"></header>
+      <div className="main"></div>
+      <footer className="bottom"><button className="btn">Click</button></footer>
     </div>
   );
 }
